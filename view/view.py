be5791219bee146b31e71796ffe5ba333ef95612
Jean-Luc Chamaa <jlchamaa@gmail.com>
2019-05-05 14:01:32 -0700

    reimplement curses wrapper so that it exists in sync with the command loop

--- view/view.py
+++ view/view.py
@@ -1,19 +1,42 @@
 import curses
+import os
+
+
 class View:
     def __init__(self):
-        curses.wrapper(self.main)
-
-    def main(self, stdscr):
-        self.stdscr = stdscr
-        self.stdscr.clear()
-        self.render_homescreen()
-        self.open = True
+        self.log_file = os.path.expanduser("~/.log_from_wfcli")
 
     def render_homescreen(self):
-        self.stdscr.addstr(2, 2, "This is the homescreen")
+        self.sc.border()
+        self.sc.addstr(2, 2, "This is the homescreen")
+
+    def log_input(self, to_log):
+        with open(self.log_file, "a+") as f:
+            f.write("{}".format(to_log))
+
 
     def send_command(self):
-        while self.open:
-            keypress = self.stdscr.getkey()
-            if keypress == "q":
-                yield ("quit_app", {})
+        try:
+            self.sc = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            try:
+                curses.start_color()
+            except:
+                pass
+            self.sc.clear()
+            self.sc.nodelay(True)
+            self.render_homescreen()
+            self.open = True
+            while self.open:
+                try:
+                    keypress = self.sc.getkey()
+                except:
+                    continue
+                self.log_input(keypress)
+                if keypress == "q":
+                    yield ("quit_app", {})
+        finally:
+            curses.echo()
+            curses.nocbreak()
+            curses.endwin()
