9211844b38250dd0a495a380de68255acb52d592
Jean-Luc Chamaa <jlchamaa@gmail.com>
2020-03-14 21:41:17 -0700

    Move the normal mode keypress mapping into the mode class

--- view/modes/normal.py
+++ view/modes/normal.py
@@ -1,10 +1,27 @@
 import curses
 import logging
-from view.keypress import key_mapping
 log = logging.getLogger("wfcli")
 
 
 class NormalMode:
+    key_mapping = {
+        (ord('d'), ord('d')): "delete_item",
+        (ord('c'),): "complete",
+        (ord('i'),): "edit_item",
+        (ord('k'),): "nav_up",
+        (ord('j'),): "nav_down",
+        (ord('h'),): "nav_left",
+        (ord('l'),): "nav_right",
+        (ord('o'),): "open_below",
+        (ord('p'),): "print_data",
+        (ord('q'),): "quit_app",
+        (ord('s'),): "save_data",
+        (ord('u'),): "undo",
+        (9,): "indent",            # TAB
+        (10,): "open_below",       # ENTER
+        (27, 91, 90): "unindent",  # SHIFT-TAB
+    }
+
     def __init__(self, sc):
         self.sc = sc
 
@@ -44,8 +61,8 @@ class NormalMode:
     def get_command(self):
         while True:
             key_combo = self.get_keypress()
-            if key_combo in key_mapping:
-                result = key_mapping[key_combo]
+            if key_combo in self.key_mapping:
+                result = self.key_mapping[key_combo]
                 return (result, {})
             else:
                 log.error("This command is dogshit - {}".format(key_combo))
